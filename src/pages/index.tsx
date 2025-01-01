cf6b59de48c6f63248457cb7217538c7d1121c57
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-01-01 22:02:06 +0900

    style : index.module.css 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,10 @@
+import style from "./index.module.css";
+
 export default function Home() {
   return (
     <>
-      <h1>안녕, Next.js!</h1>
+      <h1 className={style.h1}>안녕, Next.js!</h1>
+      <h2 className={style.h2}>Next 프로젝트 드가자</h2>
     </>
   );
 }
