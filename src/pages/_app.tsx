6236cd0dff6fa6f8705517776f17a0ffe663e96e
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-01-01 14:42:11 +0900

    test : pre-fetching 과정 분석

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,34 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
-// 모든 컴포넌들의 부모 컴포넌트
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const onClickButton = () => {
+    router.push("/test");
+  };
+
+  // prefetch 메서드를 통해 특정 페이지를 명시적으로 pre-fetching 하도록 설정
+  useEffect(() => {
+    router.prefetch("/test");
+  }, []);
+
   return (
     <>
       <header>글로벌 헤더</header>
+      <Link href={"/"}>index</Link>
+      &nbsp;
+      <Link href={"/search"} prefetch={false}>
+        search
+      </Link>
+      &nbsp;
+      <Link href={"/book/1"}>book 1</Link>
+      <div>
+        <button onClick={onClickButton}>/test 페이지로 이동</button>
+      </div>
       <Component {...pageProps} />
     </>
   );
 }
-
-// Component props는 현재 페이지 역할을 할 컴포넌트를 받는 것
-// pageProps는 Component에 전달될 props를 모두 객체로 보관한 것
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,3 +1,16 @@
+import { useRouter } from "next/router";
+
 export default function Page() {
-  return <h1>Test</h1>;
+  const router = useRouter();
+
+  const onClickButton = () => {
+    router.back();
+  };
+
+  return (
+    <>
+      <h1>Test</h1>
+      <button onClick={onClickButton}>뒤로 가기</button>
+    </>
+  );
 }
