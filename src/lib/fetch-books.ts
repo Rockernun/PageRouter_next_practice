49f2a959bb97e5587eca06d2fcec2b15861668c2
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2025-01-05 22:28:55 +0900

    feat : 검색 페이지 SSR 방식 적용

--- src/lib/fetch-books.ts
+++ src/lib/fetch-books.ts
@@ -1,7 +1,12 @@
 import { BookProps } from "../type";
 
-export default async function fetchBooks(): Promise<BookProps[]> {
-  const url = `http://localhost:12345/book`;
+export default async function fetchBooks(q?: string): Promise<BookProps[]> {
+  let url = `http://localhost:12345/book`;
+
+  // 만약 매개변수로 검색어가 전달되었다면
+  if (q) {
+    url += `/search?q=${q}`;
+  }
 
   try {
     const response = await fetch(url);
